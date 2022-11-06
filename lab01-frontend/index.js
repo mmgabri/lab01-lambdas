8dff5a486cb7374e4eb5edd6fb9229c49e36101b
mmgabri <marcelomgabriel@gmail.com>
2022-11-06 16:20:14 -0300

    Configuração app e firebase

--- lab01-frontend/index.js
+++ lab01-frontend/index.js
@@ -5,6 +5,7 @@
  import {AppRegistry} from 'react-native';
  import App from './src/navigation/App';
  import {name as appName} from './app.json';
+ import './Notifications'
  
  AppRegistry.registerComponent(appName, () => App);
  
