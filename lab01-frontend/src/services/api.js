0b4d0114d35d0d3edf986b69f38ef95efc0974a2
Marcelo Gabriel <marcelomgabriel@gmail.com>
2025-08-24 16:26:16 -0300

    Retirando credenciais

--- lab01-frontend/src/services/api.js
+++ lab01-frontend/src/services/api.js
@@ -4,7 +4,7 @@ const apiUser = axios.create({
   //baseURL: 'http://10.0.2.2:9000/users',
    baseURL: 'https://y4tny8add8.execute-api.us-east-1.amazonaws.com/Prod/users',
    headers: {
-    'x-api-key' : 'U8eT4hjyLA9GmWdyhKTO0a5Z2c9OzNWaa1flYJMj'
+    'x-api-key' : ''
   }
 });
 
@@ -12,7 +12,7 @@ const apiAnuncio = axios.create({
 // baseURL: 'http://10.0.2.2:9000/anuncios',
  baseURL: 'https://y4tny8add8.execute-api.us-east-1.amazonaws.com/Prod/anuncios',
   headers: {
-    'x-api-key' : 'U8eT4hjyLA9GmWdyhKTO0a5Z2c9OzNWaa1flYJMj'
+    'x-api-key' : ''
   }
 });
 
@@ -20,7 +20,7 @@ const apiChat = axios.create({
   //baseURL: 'http://10.0.2.2:9000/chats',
   baseURL: 'https://y4tny8add8.execute-api.us-east-1.amazonaws.com/Prod/chats',
   headers: {
-    'x-api-key': 'U8eT4hjyLA9GmWdyhKTO0a5Z2c9OzNWaa1flYJMj'
+    'x-api-key': ''
   }
 });
 
